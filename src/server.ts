1b5ec2439a6377dad6f646b186d648f9219e1e47
haithem <haithem2001@gmail.com>
2022-08-28 00:03:27 +0100

    a

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import { combineOneVideo } from "./engine";
 import { getFilesUsingPatternPath, getName } from "./utils";
 
+/** the quantity of clips and voices to use */
+const quantity = 5;
+
+
+
+
+const clappy = (arr: any[]) => arr.sort(_ => Math.random() - .5).slice(0, quantity);
 
 (async () => {
     const clips = await getFilesUsingPatternPath("./test/clips/*", __dirname)
     const voices = await getFilesUsingPatternPath("./test/voices/*", __dirname)
-    await combineOneVideo("i1.mp4", clips, voices);
+    await combineOneVideo("i1.mp4", clappy(clips), clappy(voices));
 })()
