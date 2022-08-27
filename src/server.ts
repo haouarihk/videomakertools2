5e01a24ec9f181c3a6277d189ff71482aa2e5026
haithem <haithem2001@gmail.com>
2022-08-28 00:07:26 +0100

    voices no limit

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,10 @@ import { getFilesUsingPatternPath, getName } from "./utils";
 const quantity = 5;
 
 
-
-
 const clappy = (arr: any[]) => arr.sort(_ => Math.random() - .5).slice(0, quantity);
 
 (async () => {
     const clips = await getFilesUsingPatternPath("./test/clips/*", __dirname)
     const voices = await getFilesUsingPatternPath("./test/voices/*", __dirname)
-    await combineOneVideo("i1.mp4", clappy(clips), clappy(voices));
+    await combineOneVideo("i1.mp4", clappy(clips), voices);
 })()
